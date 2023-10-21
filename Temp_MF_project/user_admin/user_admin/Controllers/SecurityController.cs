4de10ee5c613bd22a1663422e42645a3446e1dff
SBEGENJOV <seydi19981995@gmail.com>
2023-10-21 18:36:28 +0300

    21.10.2023

---
+++ Temp_MF_project/user_admin/user_admin/Controllers/SecurityController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using user_admin.Models;
+
+namespace user_admin.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        Model1Container db= new Model1Container();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(calisan calisan)
+        {
+            var result = db.calisanSet.FirstOrDefault(x => x.calisanAdSoyad == calisan.calisanAdSoyad && x.calisanTel == calisan.calisanTel);
+
+            if (result != null)
+            {
+                FormsAuthentication.SetAuthCookie(result.calisanAdSoyad, true);
+                return RedirectToAction("Index","Calisan");
+            }
+            else
+            {
+                ViewBag.mesaj = "Kullanıcı adı ve ya Şifre yalnış";
+                //kullanıcı ad:Seyit
+                //çalişan tel:555 0 555
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
